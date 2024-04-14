427451baaa9aa3ed4cfbb6fc4fc6eec9e0b1cb22
Elias Eskelinen <elias.eskelinen@pm.me>
2024-04-15 02:25:38 +0300

    nvim: add more telescope bindings

--- nvim/lua/plugins/config/telescope.lua
+++ nvim/lua/plugins/config/telescope.lua
@@ -8,6 +8,9 @@ require("telescope").setup({
 			hidden = true,
 		},
 	},
+	defaults = {
+		path_display = { "shorten" },
+	}
 })
 
 -- nice default keymaps
@@ -16,6 +19,13 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Find in files" })
 vim.keymap.set('n', '<leader>fl', builtin.buffers, { desc = "Find buffers" })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find help" })
+-- lsp stuff
+vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, { desc = "Find symbols" })
+vim.keymap.set('n', '<leader>sr', builtin.lsp_references, { desc = "Find references" })
+vim.keymap.set('n', '<leader>sd', builtin.lsp_definitions, { desc = "Find definitions" })
+vim.keymap.set('n', '<leader>st', builtin.lsp_type_definitions, { desc = "Find type definitions" })
+vim.keymap.set('n', '<leader>si', builtin.lsp_implementations, { desc = "Find implementations" })
+vim.keymap.set('n', '<leader>ts', builtin.treesitter, { desc = "Treesitter" })
 
 -- file browser
 require("telescope").load_extension("file_browser")
@@ -30,7 +40,7 @@ vim.api.nvim_set_keymap(
 	"n",
 	"<space>fb",
 	":Telescope file_browser path=%:p:h select_buffer=true<CR>",
-	{ noremap = true }
+	{ noremap = true, desc = "File browser in current directory" }
 )
 
 -- refactoring
