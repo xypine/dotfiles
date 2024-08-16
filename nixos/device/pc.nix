d00be6c0ad7ae43f5b42983b74635fcd258a3ce2
Elias Eskelinen <elias.eskelinen@pm.me>
2024-08-17 01:21:32 +0300

    nixos: add a git server for pc

--- nixos/device/pc.nix
+++ nixos/device/pc.nix
@@ -40,6 +40,25 @@
       ExecStart = "${pkgs.snapcast}/bin/snapclient -h localhost --player pulse -s 214";
     };
   };
+  # TUI Git server
+  services.soft-serve = {
+    enable = true;
+    settings = {
+      name = "EEPC";
+      ssh = {
+        listen_addr = ":23231";
+        public_url = "ssh://eepc:23231";
+      };
+      http = {
+        listen_addr = ":23232";
+        public_url = "http://eepc:23232";
+      };
+      stats.listen_addr = ":23233";
+      initial_admin_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFPPdANVtEU5YNoQepD3c1ymriPUeOXgkZUMgcNYYMUR git@eliaseskelinen.fi"
+      ];
+    };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
