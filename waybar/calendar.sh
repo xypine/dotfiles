2832c3910e02bacaeedc933aff5c4d606be4a15d
Elias Eskelinen <elias.eskelinen@pm.me>
2024-10-28 21:42:55 +0200

    waybar: add a newline between calendar entries

--- waybar/calendar.sh
+++ waybar/calendar.sh
@@ -1,4 +1,4 @@
-events_human=$(olmonokod events | jq -r '.[] | [.starts_at_human, .summary] | join(" ")' | sed -z 's/\n/\\n/g')
+events_human=$(olmonokod events | jq -r '.[] | [.starts_at_human, .summary] | join(" ")' | sed -z 's/\n/\\n\\n/g')
 
 printf "{ \"text\": \"%s\", \"tooltip\": \"%s\" }\n" \
        "_" "$events_human"
