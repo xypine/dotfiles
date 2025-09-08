fd1841b291735c4684d605bd24f0c3f9e10eb772
Elias Eskelinen <git@eliaseskelinen.fi>
2025-09-08 17:44:14 +0300

    Add fw-fanctrl

--- nixos/device/framework.nix
+++ nixos/device/framework.nix
@@ -86,6 +86,12 @@
   # Firmware upgrades
   services.fwupd.enable = true;
 
+  # Smarter fan control
+  hardware.fw-fanctrl = {
+    enable = true;
+    disableBatteryTempCheck = false;
+  };
+
   # Misc packages
   environment.systemPackages = with pkgs; [
     nvtopPackages.amd
